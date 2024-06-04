d7abcb4472e7c180f99b409d03d04d7f0c7dd57b
pablo <tonyp2hoy@gmail.com>
2024-06-03 20:28:40 -0400

    feat: adding schema/dtos/API operations

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Book } from './schemas/book.schema';
+import mongoose from 'mongoose';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(
+    @InjectModel(Book.name)
+    private bookModel: mongoose.Model<Book>,
+  ) {}
+
+  async findAll(): Promise<Book[]> {
+    const books = await this.bookModel.find();
+    return books;
+  }
+
+  async create(book: Book): Promise<Book> {
+    const res = await this.bookModel.create(book);
+    return res;
+  }
+
+  async findById(id: String): Promise<Book> {
+    const book = await this.bookModel.findById(id);
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+    return book;
+  }
+
+  async updateById(id: String, book: Book): Promise<Book> {
+    return await this.bookModel.findByIdAndUpdate(id, book, {
+      new: true,
+      runValidators: true,
+    });
+  }
+
+  async deleteById(id: String): Promise<Book> {
+    return await this.bookModel.findByIdAndDelete(id);
+  }
+}
